4113c12cf96324942ebe147aa110b1e47556d9a1
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-23 03:01:07 +0300

    completed bonus code along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+/*function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
@@ -19,6 +19,58 @@ function Form() {
       <button type="submit">Submit</button>
     </form>
   );
+}*/
+
+function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "Timothy",
+    lastName: "Mureithi",
+    admin: false,
+  });
+
+ 
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({...formData, [name]: value,});
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+//give input name attributes in order to access them as event.target.name
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+
 }
+
+
 
 export default Form;
